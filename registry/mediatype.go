4ccbd5aacc5ad5102aa2e8b922fd794e8c872f74
Shiwei Zhang <shizh@microsoft.com>
2021-02-02 19:51:40 +0800

    update with latest artifact spec
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- registry/mediatype.go
+++ registry/mediatype.go
@@ -1,9 +1,6 @@
 package registry
 
 const (
-	// MediaTypeImageIndex specifies the media type for an image index.
-	MediaTypeImageIndex = "application/vnd.oci.image.index.v2+json"
-
 	// MediaTypeNotaryConfig specifies the media type for the notary signature.
 	MediaTypeNotaryConfig = "application/vnd.cncf.notary.config.v2+jwt"
 )
