e698b393142aca2d0f1453757d877117870b7965
Rakesh Gariganti <5878554+rgnote@users.noreply.github.com>
2022-08-03 16:57:54 -0700

    Implement custom signature verification level (#84)
    * Implement custom signature verification level
    * Update custom verification with the latest spec changes
    * fix merge conflicts
    
    Signed-off-by: rgnote <5878554+rgnote@users.noreply.github.com>

--- verification/types_test.go
+++ verification/types_test.go
@@ -5,22 +5,22 @@ import (
 	"testing"
 )
 
-func TestFindVerificationLevel(t *testing.T) {
+func TestGetVerificationLevel(t *testing.T) {
 	tests := []struct {
-		verificationLevel   string
+		verificationLevel   SignatureVerification
 		wantErr             bool
 		verificationActions []VerificationAction
 	}{
-		{"strict", false, []VerificationAction{Enforced, Enforced, Enforced, Enforced, Enforced}},
-		{"permissive", false, []VerificationAction{Enforced, Enforced, Logged, Logged, Logged}},
-		{"audit", false, []VerificationAction{Enforced, Logged, Logged, Logged, Logged}},
-		{"skip", false, []VerificationAction{Skipped, Skipped, Skipped, Skipped, Skipped}},
-		{"invalid", true, []VerificationAction{}},
+		{SignatureVerification{Level: "strict"}, false, []VerificationAction{Enforced, Enforced, Enforced, Enforced, Enforced}},
+		{SignatureVerification{Level: "permissive"}, false, []VerificationAction{Enforced, Enforced, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "audit"}, false, []VerificationAction{Enforced, Logged, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "skip"}, false, []VerificationAction{Skipped, Skipped, Skipped, Skipped, Skipped}},
+		{SignatureVerification{Level: "invalid"}, true, []VerificationAction{}},
 	}
 	for i, tt := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 
-			level, err := FindVerificationLevel(tt.verificationLevel)
+			level, err := GetVerificationLevel(tt.verificationLevel)
 
 			if tt.wantErr != (err != nil) {
 				t.Fatalf("TestFindVerificationLevel Error: %q WantErr: %v", err, tt.wantErr)
@@ -28,6 +28,56 @@ func TestFindVerificationLevel(t *testing.T) {
 				for index, action := range tt.verificationActions {
 					if action != level.VerificationMap[VerificationTypes[index]] {
 						t.Errorf("%q verification action should be %q for Verification Level %q", VerificationTypes[index], action, tt.verificationLevel)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestCustomVerificationLevel(t *testing.T) {
+	tests := []struct {
+		customVerification  SignatureVerification
+		wantErr             bool
+		verificationActions []VerificationAction
+	}{
+		{SignatureVerification{Level: "strict", Override: map[string]string{"integrity": "log"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"authenticity": "skip"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"authenticTimestamp": "skip"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"expiry": "skip"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "skip", Override: map[string]string{"authenticity": "log"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "invalid", Override: map[string]string{"authenticity": "log"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"invalid": "log"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"authenticity": "invalid"}}, true, []VerificationAction{}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"authenticity": "log"}}, false, []VerificationAction{Enforced, Logged, Enforced, Enforced, Enforced}},
+		{SignatureVerification{Level: "permissive", Override: map[string]string{"authenticity": "log"}}, false, []VerificationAction{Enforced, Logged, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "audit", Override: map[string]string{"authenticity": "log"}}, false, []VerificationAction{Enforced, Logged, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"expiry": "log"}}, false, []VerificationAction{Enforced, Enforced, Enforced, Logged, Enforced}},
+		{SignatureVerification{Level: "permissive", Override: map[string]string{"expiry": "log"}}, false, []VerificationAction{Enforced, Enforced, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "audit", Override: map[string]string{"expiry": "log"}}, false, []VerificationAction{Enforced, Logged, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"revocation": "log"}}, false, []VerificationAction{Enforced, Enforced, Enforced, Enforced, Logged}},
+		{SignatureVerification{Level: "permissive", Override: map[string]string{"revocation": "log"}}, false, []VerificationAction{Enforced, Enforced, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "audit", Override: map[string]string{"revocation": "log"}}, false, []VerificationAction{Enforced, Logged, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"revocation": "skip"}}, false, []VerificationAction{Enforced, Enforced, Enforced, Enforced, Skipped}},
+		{SignatureVerification{Level: "permissive", Override: map[string]string{"revocation": "skip"}}, false, []VerificationAction{Enforced, Enforced, Logged, Logged, Skipped}},
+		{SignatureVerification{Level: "audit", Override: map[string]string{"revocation": "skip"}}, false, []VerificationAction{Enforced, Logged, Logged, Logged, Skipped}},
+		{SignatureVerification{Level: "permissive", Override: map[string]string{"authenticTimestamp": "log"}}, false, []VerificationAction{Enforced, Enforced, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "audit", Override: map[string]string{"authenticTimestamp": "log"}}, false, []VerificationAction{Enforced, Logged, Logged, Logged, Logged}},
+		{SignatureVerification{Level: "strict", Override: map[string]string{"authenticTimestamp": "log"}}, false, []VerificationAction{Enforced, Enforced, Logged, Enforced, Enforced}},
+	}
+	for i, tt := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			level, err := GetVerificationLevel(tt.customVerification)
+
+			if tt.wantErr != (err != nil) {
+				t.Fatalf("TestCustomVerificationLevel Error: %q WantErr: %v", err, tt.wantErr)
+			} else {
+				if !tt.wantErr && len(tt.verificationActions) == 0 {
+					t.Errorf("test case isn't configured with VerificationActions")
+				}
+				for index, action := range tt.verificationActions {
+					if action != level.VerificationMap[VerificationTypes[index]] {
+						t.Errorf("%q verification action should be %q for custom verification %q", VerificationTypes[index], action, tt.customVerification)
 					}
 				}
 			}
