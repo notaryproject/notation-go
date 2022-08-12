13b25d11dbc0e8d004b60078745eb2192a0e6a4e
vaninrao10 <111005862+vaninrao10@users.noreply.github.com>
2022-08-12 14:35:25 -0400

    Update version.go

--- internal/Version/version.go
+++ internal/Version/version.go
@@ -1,1 +1,17 @@
+package version
 
+var (
+	// Version shows the current notation-go version, optionally with pre-release.
+	Version = "v1.0.0-alpha.3"
+
+	// BuildMetadata stores the build metadata.
+	BuildMetadata = "unreleased"
+)
+
+// GetVersion returns the version string in SemVer 2.
+func GetVersion() string {
+	if BuildMetadata == "" {
+		return Version
+	}
+	return Version + "+" + BuildMetadata
+}
