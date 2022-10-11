ebd12d2eeac5803cf79d35d0c4c8e7e957ec3450
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-11 15:35:36 +0800

    minor update
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- core/signature/cose/envelope.go
+++ core/signature/cose/envelope.go
@@ -2,5 +2,5 @@ package cose
 
 import "github.com/notaryproject/notation-core-go/signature/cose"
 
-// MediaTypeEnvelope is the COSE signature envelope blob mediaType
+// MediaTypeEnvelope is the COSE signature envelope blob mediaType.
 const MediaTypeEnvelope string = cose.MediaTypeEnvelope
