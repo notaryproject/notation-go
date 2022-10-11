964df84cbf45bc6907d83e819f64df6e4fcac8aa
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-11 16:43:07 +0800

    removed const type
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- core/signature/cose/envelope.go
+++ core/signature/cose/envelope.go
@@ -3,4 +3,4 @@ package cose
 import "github.com/notaryproject/notation-core-go/signature/cose"
 
 // MediaTypeEnvelope is the COSE signature envelope blob mediaType.
-const MediaTypeEnvelope string = cose.MediaTypeEnvelope
+const MediaTypeEnvelope = cose.MediaTypeEnvelope
