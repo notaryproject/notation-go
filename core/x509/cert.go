2a00ba62ab436eb6089bcc398fa137cfec904e2e
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-11 12:31:05 +0800

    added alias of notation-core-go
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- core/x509/cert.go
+++ core/x509/cert.go
@@ -1,1 +1,12 @@
 package x509
+
+import (
+	"crypto/x509"
+
+	corex509 "github.com/notaryproject/notation-core-go/x509"
+)
+
+// ReadCertificateFile reads a certificate file (support PEM and DER formats).
+func ReadCertificateFile(path string) ([]*x509.Certificate, error) {
+	return corex509.ReadCertificateFile(path)
+}
