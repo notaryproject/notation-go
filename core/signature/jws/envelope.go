85a12b03e9f6664a2664e1fdb941526522ae4f84
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-11 14:31:03 +0800

    add comments
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- core/signature/jws/envelope.go
+++ core/signature/jws/envelope.go
@@ -2,4 +2,5 @@ package jws
 
 import "github.com/notaryproject/notation-core-go/signature/jws"
 
+// MediaTypeEnvelope defines the media type name of JWS envelope.
 const MediaTypeEnvelope = jws.MediaTypeEnvelope
