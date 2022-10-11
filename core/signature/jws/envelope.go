316e1f9a8c4bf40737b535f3ae0d71200f7f6bb3
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-11 16:43:49 +0800

    removed constant type
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- core/signature/jws/envelope.go
+++ core/signature/jws/envelope.go
@@ -3,4 +3,4 @@ package jws
 import "github.com/notaryproject/notation-core-go/signature/jws"
 
 // MediaTypeEnvelope defines the media type name of JWS envelope.
-const MediaTypeEnvelope string = jws.MediaTypeEnvelope
+const MediaTypeEnvelope = jws.MediaTypeEnvelope
