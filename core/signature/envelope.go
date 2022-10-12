6f61d2fa9dfc2dfaf3daf0754af821ac9850a976
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-12 13:15:01 +0800

    minor update of the notation-core-go alias inside notation-go (#169)
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- core/signature/envelope.go
+++ core/signature/envelope.go
@@ -1,8 +1,9 @@
 package signature
 
-import (
-	"github.com/notaryproject/notation-core-go/signature"
-)
+import "github.com/notaryproject/notation-core-go/signature"
+
+// Envelope provides basic functions to manipulate signatures.
+type Envelope = signature.Envelope
 
 // RegisteredEnvelopeTypes lists registered envelope media types.
 func RegisteredEnvelopeTypes() []string {
@@ -11,6 +12,6 @@ func RegisteredEnvelopeTypes() []string {
 
 // ParseEnvelope generates an envelope for given envelope bytes with specified
 // media type.
-func ParseEnvelope(mediaType string, envelopeBytes []byte) (signature.Envelope, error) {
+func ParseEnvelope(mediaType string, envelopeBytes []byte) (Envelope, error) {
 	return signature.ParseEnvelope(mediaType, envelopeBytes)
 }
