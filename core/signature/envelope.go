009913e19ebf5e825809972f13d696d5eec0195a
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-12 10:45:26 +0800

    update per code review
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- core/signature/envelope.go
+++ core/signature/envelope.go
@@ -4,6 +4,9 @@ import (
 	"github.com/notaryproject/notation-core-go/signature"
 )
 
+// Envelope provides basic functions to manipulate signatures.
+type Envelope = signature.Envelope
+
 // RegisteredEnvelopeTypes lists registered envelope media types.
 func RegisteredEnvelopeTypes() []string {
 	return signature.RegisteredEnvelopeTypes()
@@ -11,6 +14,6 @@ func RegisteredEnvelopeTypes() []string {
 
 // ParseEnvelope generates an envelope for given envelope bytes with specified
 // media type.
-func ParseEnvelope(mediaType string, envelopeBytes []byte) (signature.Envelope, error) {
+func ParseEnvelope(mediaType string, envelopeBytes []byte) (Envelope, error) {
 	return signature.ParseEnvelope(mediaType, envelopeBytes)
 }
