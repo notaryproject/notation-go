767fee2f1233cc0a0d1b4bdf77c7efcfac4efa16
Patrick Zheng <patrickzheng@microsoft.com>
2022-10-20 17:12:52 +0800

    clean up
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- crypto/jwsutil/errors.go
+++
@@ -1,9 +0,0 @@
-package jwsutil
-
-import "errors"
-
-// Common errors
-var (
-	ErrInvalidCompactSerialization = errors.New("invalid compact serialization")
-	ErrInvalidJSONSerialization    = errors.New("invalid JSON serialization")
-)
