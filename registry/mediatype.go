0f397a5208810fb07c8b1698ffb7a935edf62de1
Patrick Zheng <patrickzheng@microsoft.com>
2022-12-07 09:16:03 +0800

    update
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- registry/mediatype.go
+++ registry/mediatype.go
@@ -1,4 +1,5 @@
 package registry
 
 // ArtifactTypeNotation specifies the artifact type for a notation object.
+// spec: https://github.com/notaryproject/notaryproject/blob/efc828223710f99ab9639d2d0f72d59036a8e80c/specs/signature-specification.md#storage
 const ArtifactTypeNotation = "application/vnd.cncf.notary.signature"
