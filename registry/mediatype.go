146ffbb5ec725568f08ac3e010a7dc1e651cdc93
Patrick Zheng <patrickzheng@microsoft.com>
2022-12-07 08:53:29 +0800

    update
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- registry/mediatype.go
+++ registry/mediatype.go
@@ -1,4 +1,4 @@
 package registry
 
 // ArtifactTypeNotation specifies the artifact type for a notation object.
-const ArtifactTypeNotation = "application/vnd.cncf.notary.v2.signature"
+const ArtifactTypeNotation = "application/vnd.cncf.notary.signature"
