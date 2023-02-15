cac9f1a5e9f2fe686821f00d5b0824b92f047a54
Byron Chien <byronc@ucla.edu>
2023-02-14 21:01:35 -0800

    Remove comment
    Signed-off-by: Byron Chien <byronc@ucla.edu>

--- internal/pkix/pkix.go
+++ internal/pkix/pkix.go
@@ -9,9 +9,6 @@ import (
 
 // ParseDistinguishedName parses a DN name and validates Notary V2 rules
 func ParseDistinguishedName(name string) (map[string]string, error) {
-	// To circumvent an issue in go-asn1-ber/asn1-ber where it allocates large
-	// amount of memory for decoding ber.
-	// For more information please look at https://github.com/notaryproject/notation-go/issues/276
 	if strings.Contains(name, "=#") {
 		return nil, fmt.Errorf("unsupported distinguished name (DN) %q: notation does not support x509.subject identities containing \"=#\"", name)
 	}
