d52ca7162a492277aea8cff5aebcedf4f567f536
Junjie Gao <junjiegao@microsoft.com>
2024-01-18 09:24:19 +0800

    fix: improve plugin error message (#371)
    - added `PluginUnknownError`, `PluginValidityError`,
    `PluginDirectoryError`. For each error types, Notation CLI should
    provide a recommanded suggestion to solve them.
    - improved error message
    
    Resolve part of: https://github.com/notaryproject/notation/issues/824
    Resolve part of: https://github.com/notaryproject/notation/issues/867
    
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>
    
    ---------
    
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- plugin/errors.go
+++ plugin/errors.go
@@ -20,7 +20,7 @@ import "errors"
 var ErrNotCompliant = errors.New("plugin not compliant")
 
 // ErrNotRegularFile is returned when the plugin file is not an regular file.
-var ErrNotRegularFile = errors.New("not regular file")
+var ErrNotRegularFile = errors.New("plugin executable file is not a regular file")
 
 // PluginDowngradeError is returned when installing a plugin with version
 // lower than the exisiting plugin version.
@@ -28,6 +28,7 @@ type PluginDowngradeError struct {
 	Msg string
 }
 
+// Error returns the error message.
 func (e PluginDowngradeError) Error() string {
 	if e.Msg != "" {
 		return e.Msg
@@ -41,9 +42,39 @@ type InstallEqualVersionError struct {
 	Msg string
 }
 
+// Error returns the error message.
 func (e InstallEqualVersionError) Error() string {
 	if e.Msg != "" {
 		return e.Msg
 	}
 	return "installing plugin with version equal to the existing plugin version"
 }
+
+// PluginMalformedError is used when there is an issue with plugin and
+// should be fixed by plugin developers.
+type PluginMalformedError struct {
+	Msg        string
+	InnerError error
+}
+
+// Error returns the error message.
+func (e PluginMalformedError) Error() string {
+	if e.Msg != "" {
+		return e.Msg
+	}
+	return e.InnerError.Error()
+}
+
+// Unwrap returns the inner error.
+func (e PluginMalformedError) Unwrap() error {
+	return e.InnerError
+}
+
+// PluginDirectoryWalkError is used when there is an issue with plugins directory
+// and should suggest user to check the permission of plugin directory.
+type PluginDirectoryWalkError error
+
+// PluginExecutableFileError is used when there is an issue with plugin
+// executable file and should suggest user to check the existence, permission
+// and platform/arch compatibility of plugin.
+type PluginExecutableFileError error
