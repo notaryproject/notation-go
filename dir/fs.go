4d76f9a41595021b71b5d36559a29e8def50aa47
JasonTheDeveloper <jagoodse@microsoft.com>
2024-09-04 17:05:30 +1000

    fix: dir no longer panics when HOME and XDG_CONFIG_HOME are not set (#449)
    This PR addresses the issue #446
    
    In this PR I:
    
    - I removed the `init()` function from `dir/path`
    - When `userConfigDir()` returns an error, instead of `panic(err)` I
    default to the current directory instead
    - Split `loadUserPath()` into two new functions used to setup and return
    the values for `UserConfigDir` and `UserLibexecDir`
    - Added additional unit tests for the two new functions and to test the
    default directory is used when `HOME` is set to `""`
    
    ---------
    
    Signed-off-by: Jason <jagoodse@microsoft.com>
    Signed-off-by: JasonTheDeveloper <jagoodse@microsoft.com>
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>
    Co-authored-by: Shiwei Zhang <shizh@microsoft.com>
    Co-authored-by: Patrick Zheng <patrickzheng@microsoft.com>

--- dir/fs.go
+++ dir/fs.go
@@ -51,10 +51,10 @@ func NewSysFS(root string) SysFS {
 
 // ConfigFS is the config SysFS
 func ConfigFS() SysFS {
-	return NewSysFS(UserConfigDir)
+	return NewSysFS(userConfigDirPath())
 }
 
 // PluginFS is the plugin SysFS
 func PluginFS() SysFS {
-	return NewSysFS(filepath.Join(UserLibexecDir, PathPlugins))
+	return NewSysFS(filepath.Join(userLibexecDirPath(), PathPlugins))
 }
