595c710082d2764807cba1ae6d8ee4420f0d9a51
Junjie Gao <junjiegao@microsoft.com>
2024-09-05 03:20:32 +0000

    bump: release v1.1.2
    Signed-off-by: Junjie Gao <junjiegao@microsoft.com>

--- signer/signer.go
+++ signer/signer.go
@@ -34,7 +34,7 @@ import (
 )
 
 // signingAgent is the unprotected header field used by signature.
-const signingAgent = "notation-go/1.1.2+unreleased"
+const signingAgent = "notation-go/1.1.2"
 
 // genericSigner implements notation.Signer and embeds signature.Signer
 type genericSigner struct {
