f63defd138f83e5b86e2bc9c4dc65972af9d19fc
Patrick Zheng <patrickzheng@microsoft.com>
2024-12-12 10:43:54 +0800

    bump: release v1.3.0-rc.2
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- signer/signer.go
+++ signer/signer.go
@@ -34,7 +34,7 @@ import (
 )
 
 // signingAgent is the unprotected header field used by signature.
-const signingAgent = "notation-go/1.3.0-rc.1"
+const signingAgent = "notation-go/1.3.0-rc.2"
 
 // GenericSigner implements notation.Signer and embeds signature.Signer
 type GenericSigner struct {
