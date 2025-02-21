d909a1909dcd0e53f23d7455e787fedc54ff2f89
Patrick Zheng <patrickzheng@microsoft.com>
2025-02-21 10:53:31 +0800

    timestamping fix
    Signed-off-by: Patrick Zheng <patrickzheng@microsoft.com>

--- verifier/verifier.go
+++ verifier/verifier.go
@@ -1082,9 +1082,6 @@ func verifyTimestamp(ctx context.Context, policyName string, trustStores []strin
 	if err != nil {
 		return fmt.Errorf("failed to verify the timestamp countersignature with error: %w", err)
 	}
-	if !timestamp.BoundedAfter(signerInfo.SignedAttributes.SigningTime) {
-		return fmt.Errorf("timestamp %s is not bounded after the signing time %q", timestamp.Format(time.RFC3339), signerInfo.SignedAttributes.SigningTime)
-	}
 
 	// 3. Validate timestamping certificate chain
 	logger.Debug("Validating timestamping certificate chain...")
